ebcc2662283197c4b3acc119e2cbece6d3ed1552
Edgarcillo2k <44873692+Edgarcillo2k@users.noreply.github.com>
2020-08-24 11:55:05 -0600

    Update RegressionReducer.java

--- mapreduce/src/main/java/reducers/RegressionReducer.java
+++ mapreduce/src/main/java/reducers/RegressionReducer.java
@@ -9,42 +9,21 @@ import java.io.IOException;
 public class RegressionReducer extends Reducer<Text, RegressionVariablesWrapper, Text, Text> {
     @Override
     public void reduce(Text key, Iterable<RegressionVariablesWrapper> values, Context context) throws IOException, InterruptedException {
-        Double end = 0.0;
-        Double init;
         double sumY = 0;
         double sumX = 0;
         double sumXY = 0;
-        double sumX2Y = 0;
         double sumX2 = 0;
-        double sumX3 = 0;
-        double sumX4 = 0;
         double n = 0;
         for(RegressionVariablesWrapper value : values) {
-            init = value.getY();
-            if(n > 0){
-                double y = end / init;
-                sumX += value.getX();
-                sumY += y;
-                sumX2 += Math.pow(value.getX(),2);
-                sumX3 += Math.pow(value.getX(),3);
-                sumX4 += Math.pow(value.getX(),4);
-                sumXY += value.getX() * y;
-                sumX2Y += Math.pow(value.getX(),2) * y;
-            }
-            end = value.getX();
+            sumX += value.getX();
+            sumY += value.getY();
+            sumX2 += Math.pow(value.getX(),2);
+            sumXY += value.getX() * value.getY();
             n++;
         }
-        n--;
-        Double b = ( (sumXY - ((sumX*sumY)/n))*(sumX4 - ((sumX2*sumX2)/n)) - (sumX2Y - ((sumX2*sumY)/n)) * (sumX3 - ((sumX2*sumX)/n)) ) /
-                ( (sumX2 - ((sumX*sumX)/n)) * (sumX4 - ((sumX2*sumX2)/n)) - Math.pow((sumX3 - ((sumX2*sumX)/n)),2));
-        Double c = (  (sumX2 - ((sumX*sumX)/n)) * (sumX2Y - ((sumX2*sumY)/n)) - (sumX3 - ((sumX2*sumX)/n)) * (sumXY - ((sumX*sumY)/n)) )  /
-                ( (sumX2 - ((sumX*sumX)/n)) * (sumX4 - ((sumX2*sumX2)/n)) - Math.pow((sumX3 - ((sumX2*sumX)/n)),2) );
-        Double a = (sumY - b * sumX - c * sumX2) / n;
-        String bString = b.toString();//y = a + bx + cx^2
-        String cString = c.toString();
-        String aString = a.toString();
-
-        context.write(key, new Text( aString.concat("\t").concat(bString).concat("\t").concat(cString)));
+        Double b = ((n* sumXY)-(sumX*sumY))/((n*sumX2)-(sumX*sumX));
+        String bString = b.toString();
+        context.write(key, new Text(bString));
 
     }
 }
